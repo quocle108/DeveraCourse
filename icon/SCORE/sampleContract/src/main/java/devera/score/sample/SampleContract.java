3893b5e098ac4be99232b4434c31cb9606ef35d9
Dan Tran <dantn93@gmail.com>
2021-12-04 04:00:54 +0700

    Crowdsale contract

--- icon/SCORE/sampleContract/src/main/java/devera/score/sample/SampleContract.java
+++ icon/SCORE/sampleContract/src/main/java/devera/score/sample/SampleContract.java
@@ -12,42 +12,22 @@ import score.annotation.Optional;
 
 import java.math.BigInteger;
 
-public class SampleContract implements ISampleContract {
-    private final String name = "sample contract";
-    protected static final Address address = new Address(new byte[Address.LENGTH]);
+public class SampleContract {
     private final DictDB<Address, BigInteger> balances = Context.newDictDB("balances", BigInteger.class);
-    private final VarDB<BigInteger> vardbExample = Context.newVarDB("varDbExample", BigInteger.class);
-    private final ArrayDB<Long> arrayDbExample = Context.newArrayDB("arrayDbExample", Long.class);
-    private final BranchDB<String, DictDB<Address, BigInteger>> branchDBExample = Context.newBranchDB("branchDBExample", BigInteger.class);
-
-    public SampleContract(String _pram1, String _param2, int _param3) {
+    public SampleContract(String _param1, String _param2, int _param3) {
         // implement
     }
 
     @External(readonly=true)
-    public String readMethod1() {
-        return name;
+    public BigInteger balanceOf(Address _owner) {
+        return this.balances.getOrDefault(_owner, BigInteger.ZERO);
     }
 
-    @External(readonly=true)
-    public Address readMethod2() {
-        return address;
-    }
 
     @External
-    public void externalMethod1(Address _param1, Address _param2, @Optional byte[] _param3) {
+    public void tokenFallback(Address _from, BigInteger _value, byte[] dataBytes) {
         // impelemt external method
-        Event1(_param1, _param2, BigInteger.valueOf(10000), _param3);
+        BigInteger currentBalance = this.balances.getOrDefault(_from, BigInteger.ZERO);
+        this.balances.set(_from, currentBalance.add(_value));
     }
-
-    protected void _internalMethod1(Address _param1, BigInteger _param2) {
-        // implement internal method
-    }
-
-    private void _internalMethod2(Address _param1, BigInteger _param2) {
-        // implement internal method
-    }
-
-    @EventLog(indexed=3)
-    public void Event1(Address _param1, Address _param2, BigInteger _param3, byte[] _param4) {}
 }
